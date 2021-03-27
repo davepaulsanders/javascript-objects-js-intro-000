0497e484907ac35a57485a6539405e5a43552a22
David P Sanders <davepaulsanders@gmail.com>
2021-03-27 17:05:59 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { brandNew: "Sic Transit Gloria" }
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign{{playlist}, {artistName: songTitle}
+  return Object.assign({playlist}, {artistName: songTitle})
 }
