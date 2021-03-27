36d5f7596383709e4aaf85447e82d5996e78f30b
David P Sanders <davepaulsanders@gmail.com>
2021-03-27 17:25:13 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { artistNam: "Sic Transit Gloria" }
 
 function updatePlaylist(playlist, artistName, songTitle){
-  Object.assign({}, [playlist], {[artistName]: songTitle })
-  return obj
+  playlist['artistName'] = 'songTitle'
+
 }
